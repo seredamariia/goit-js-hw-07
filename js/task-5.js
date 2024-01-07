28714b4696d59e9db2e1d074ca49cae69685492e
Mariia Sereda <seredamariiav@gmail.com>
2024-01-07 22:46:52 +0200

    Added task 5 solution

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyElem = document.body;
+const spanColor = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+changeColorButton.addEventListener('click', () => {
+  const randomHexColor = getRandomHexColor();
+  bodyElem.style.backgroundColor = randomHexColor;
+  spanColor.textContent = randomHexColor;
+});
