cdec6e77836cc99a18ea868a753f08623eb41999
Mariia Sereda <seredamariiav@gmail.com>
2024-01-07 23:34:15 +0200

    Added task 6 solution

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numberInput = document.querySelector('input');
+const dataCreateButton = document.querySelector('[data-create]');
+const dataDestroyButton = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const numAmount = parseInt(amount);
+  if (numAmount >= 1 && numAmount <= 100) {
+    destroyBoxes();
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesDiv.appendChild(box);
+      size += 10;
+    }
+    numberInput.value = '';
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
+dataCreateButton.addEventListener('click', () =>
+  createBoxes(numberInput.value)
+);
+dataDestroyButton.addEventListener('click', destroyBoxes);
