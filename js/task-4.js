f0df2408c86724e1bb86584bfa1e8fb24418f5a7
Mariia Sereda <seredamariiav@gmail.com>
2024-01-07 22:39:33 +0200

    Added task 4 solution

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const emailValue = loginForm.elements.email.value.trim();
+  const passwordValue = loginForm.elements.password.value.trim();
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const data = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(data);
+    loginForm.reset();
+  }
+});
